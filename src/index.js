2a318b95cc78b9231c14e7b80ce17d7443ddfecf
Angelina German <angyricci.art@gmail.com>
2020-03-12 20:25:08 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let result = '';
+    let previousResult = '';
+
+    result = str;
+
+    while (result.length > 0) {
+        result = result.replace(/\(\)|{}|\[\]|\|\||12|34|56|77|88/g, '');
+
+        if (result === previousResult) {
+            break;
+        }
+
+        previousResult = result;
+    }
+
+    return result.length === 0;
 }
